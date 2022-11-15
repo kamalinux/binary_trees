f94dc4f88d48913967a51c35eed286ef79882c4e
Kamal Hussein <kamalinuxx@gmail.com>
2022-11-15 11:42:31 -0800

    Fixing 0-binary_tree_node.c file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,16 +8,16 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *newNode;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
 		return (NULL);
 
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
-	return (new_node);
+	return (newNode);
 }
