660f5a4304e284a8af2f0bcbf3df2f8cebbf3864
Kamal Hussein <kamalinuxx@gmail.com>
2022-11-18 14:52:01 -0800

    Fixing 121-avl_insert.c file

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,106 +1,75 @@
 #include "binary_trees.h"
 
 /**
- * check_balance - checks the balance of each node
- * @node: pointer to the node
- * @value: input value
- * Return: no return
+ * avl_rotator - rotate freshly inserted AVL tree if necessary
+ * @tmp: parent of the freshly inserted node.
+ * @lower_node: flag indicating the position of the freshly inserted node,
+ *  1 if on left of its parent, -1 if on right
  */
-void check_balance(avl_t **node, int value)
+void avl_rotator(avl_t *tmp, int lower_node)
 {
-	int balance;
+	int avl_balance;
 
-	balance = binary_tree_balance(*node);
-
-	if (balance > 1 && value < (*node)->left->n)
+	while (tmp)
 	{
-		*node = binary_tree_rotate_right(*node);
-		return;
-	}
-
-	if (balance < -1 && value > (*node)->right->n)
-	{
-		*node = binary_tree_rotate_left(*node);
-		return;
-	}
-
-	if (balance > 1 && value > (*node)->left->n)
-	{
-		(*node)->left = binary_tree_rotate_left((*node)->left);
-		*node = binary_tree_rotate_right(*node);
-		return;
-	}
-
-	if (balance < -1 && value < (*node)->right->n)
-	{
-		(*node)->right = binary_tree_rotate_right((*node)->right);
-		*node = binary_tree_rotate_left(*node);
-		return;
+		avl_balance = binary_tree_balance(tmp);
+		if (avl_balance > 1 && lower_node == 1)
+			binary_tree_rotate_right(tmp); /*case left left, rotates right*/
+		else if (avl_balance > 1 && lower_node == -1)
+		{
+			binary_tree_rotate_left(tmp->left);
+			binary_tree_rotate_right(tmp); /*case left right, rotates left then right*/
+		}
+		else if (avl_balance < -1 && lower_node == 1)
+		{
+			binary_tree_rotate_right(tmp->right);
+			binary_tree_rotate_left(tmp); /*case right left, rotates right then left*/
+		}
+		else if (avl_balance < -1 && lower_node == -1)
+			binary_tree_rotate_left(tmp); /*case right right, rotates left*/
+		avl_balance = binary_tree_balance(tmp);
+		if (avl_balance <= 1 && avl_balance >= -1)
+			tmp = tmp->parent;
 	}
 }
 
 /**
- * avl_in - checks if node is inserted
- * @tree: tree root
- * @value: node value
- * Return: pointer to the new node
- */
-avl_t *avl_in(avl_t **tree, int value)
-{
-	avl_t *node;
-
-	if (value < (*tree)->n)
-	{
-		if ((*tree)->left == NULL)
-		{
-			(*tree)->left = binary_tree_node(*tree, value);
-			return ((*tree)->left);
-		}
-		else
-		{
-			node = avl_in(&((*tree)->left), value);
-			if (node)
-				check_balance(tree, value);
-			return (node);
-		}
-	}
-
-	if (value > (*tree)->n)
-	{
-		if ((*tree)->right == NULL)
-		{
-			(*tree)->right = binary_tree_node(*tree, value);
-			return ((*tree)->right);
-		}
-		else
-		{
-			node = avl_in(&((*tree)->right), value);
-			if (node)
-				check_balance(tree, value);
-			return (node);
-		}
-	}
-
-	return (NULL);
-}
-
-/**
- * avl_insert - inserts a value in a AVL Tree
- * @tree: tree root
- * @value: node value
- * Return: pointer to the new node
+ * avl_insert - function that inserts a value in an AVL Tree
+ * @tree: pointer to the root node of the tree to traverse
+ * @value: value to store in the new node
+ * Return: ptr to the created node, or NULL on failure
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	avl_t *node;
+	int lower_node = 0;
+	avl_t *tmp = NULL, *ret_node = NULL;
 
 	if (*tree == NULL)
+		return (*tree = binary_tree_node(*tree, value));
+	tmp = *tree;
+	while (tmp)
 	{
-		*tree = binary_tree_node(NULL, value);
-		return (*tree);
+		if ((tmp->n > value) && tmp->left)
+			tmp = tmp->left;
+		else if ((tmp->n < value) && tmp->right)
+			tmp = tmp->right;
+		else if (tmp->n == value)
+			return (NULL);
+		else if (tmp->n > value) /*insert to the left*/
+		{
+			tmp->left = binary_tree_node(tmp, value);
+			ret_node = tmp->left, lower_node = 1;
+			break;
+		}
+		else if (tmp->n < value) /*insert to the right*/
+		{
+			tmp->right = binary_tree_node(tmp, value);
+			ret_node = tmp->right, lower_node = -1;
+			break;
+		}
 	}
-
-	node = avl_in(tree, value);
-
-	return (node);
+	avl_rotator(tmp, lower_node);
+	while ((*tree)->parent)
+		*tree = (*tree)->parent;
+	return (ret_node);
 }
